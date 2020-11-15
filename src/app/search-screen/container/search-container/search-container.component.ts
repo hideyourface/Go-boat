a025002d30060ed17bc2a091fbeeac5b56d3611d
Barbara Pugliatti <pugliatti.barbara@gmail.com>
2020-11-15 19:08:13 +0100

    search screen ux

--- src/app/search-screen/container/search-container/search-container.component.ts
+++ src/app/search-screen/container/search-container/search-container.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { DeviceControllerService } from 'src/app/shared/services/device-controller.service';
 import { SearchServiceService } from 'src/app/shared/services/search-service.service';
-import { faChevronRight, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight, faChevronDown, faUser, faShip, faAnchor, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -13,7 +13,10 @@ export class SearchContainerComponent implements OnInit {
   public isMobile = true;
   public faChevronRight = faChevronRight;
   public faChevronDown = faChevronDown;
-
+  public faUser = faUser;
+  public faShip = faShip;
+  public faSearch = faSearch;
+  public faAnchor = faAnchor;
 
   constructor(private deviceService : DeviceControllerService, private search : SearchServiceService) {
     if(!this.deviceService.deviceService.isMobile()){
