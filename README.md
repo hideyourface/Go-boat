376c1bbe18bb42d76d8e423004ecd26fc65370c0
Barbara Pugliatti <pugliatti.barbara@gmail.com>
2021-02-08 22:17:26 +0100

    go boat read me and versioning upgrade

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Boat
+Boat was a project for a fast search and app tool simulator in Angular. 
+
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.8.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'click-and-boat'`, () => {
+  it(`should have as title 'go-boat'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('click-and-boat');
+    expect(app.title).toEqual('go-boat');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('click-and-boat app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('go-boat app is running!');
   });
 });
